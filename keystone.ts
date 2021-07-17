7bbbc2b33d34617e0da72ddbfbd0ca46f3fbc015
jatwing <jatwing@pm.me>
2021-07-17 16:25:13 +0800

    1. use dotenv to write environment variables in .env file. 2. add .keystone/admin into .gitignore. 3. later we will use pm2 to deploy

--- keystone.ts
+++ keystone.ts
@@ -1,30 +1,32 @@
-import { config } from '@keystone-next/keystone/schema';
-import { statelessSessions } from '@keystone-next/keystone/session';
-import { createAuth } from '@keystone-next/auth';
+import { config } from "@keystone-next/keystone/schema";
+import { statelessSessions } from "@keystone-next/keystone/session";
+import { createAuth } from "@keystone-next/auth";
 
-import { lists } from './schema';
+import { lists } from "./schema";
+
+require("dotenv").config({ path: ".env" });
 
 let sessionSecret = process.env.SESSION_SECRET;
 
 if (!sessionSecret) {
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === "production") {
     throw new Error(
-      'The SESSION_SECRET environment variable must be set in production'
+      "The SESSION_SECRET environment variable must be set in production"
     );
   } else {
-    sessionSecret = '-- DEV COOKIE SECRET; CHANGE ME --';
+    sessionSecret = "-- DEV COOKIE SECRET; CHANGE ME --";
   }
 }
 
 let sessionMaxAge = 60 * 60 * 24 * 30; // 30 days
 
 const { withAuth } = createAuth({
-  listKey: 'User',
-  identityField: 'email',
-  secretField: 'password',
-  sessionData: 'name',
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
+  sessionData: "name",
   initFirstItem: {
-    fields: ['name', 'email', 'password'],
+    fields: ["name", "email", "password"],
   },
 });
 
@@ -37,19 +39,22 @@ export default withAuth(
   config({
     lists,
     db: {
-      adapter: 'prisma_postgresql',
-      url: process.env.DATABASE_URL || 'postgres://react-notes:react-notes@52.199.173.53:5432/react-notes',
+      adapter: "prisma_postgresql",
+      url: process.env.DATABASE_URL,
     },
     ui: {
       isAccessAllowed: (context) => !!context.session?.data,
     },
-    images: {
-      upload: 'local',
-      local: {
-        storagePath: 'public/images',
-        baseUrl: '/images'
-      }
+    server: {
+      port: process.env.PORT,
     },
     session,
+    images: {
+      upload: "local",
+      local: {
+        storagePath: "public/images",
+        baseUrl: "/images",
+      },
+    },
   })
 );
